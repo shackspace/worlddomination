9dab7fd447a4761dcf349755faec646744f9e83f
momo <github@mail.hackathon.de>
2015-03-25 11:40:24 +0100

    get urls from spaceapi.net directory

--- coordinates.py
+++ coordinates.py
@@ -3,38 +3,45 @@
 import json
 import requests
 
+
 def main():
     spaces = []
-    with open("urls.txt", 'r') as file:
-        urls = file.readlines()
-        for line in urls:
-            url = line.strip()
-            url = url.split(':', 1)
-            space = {}
-            space['name'] = url[0]
-            space['url'] = url[1]
-            spaces.append(space)
+
+    data = requests.get('http://spaceapi.net/directory.json?api=0.13')
+    data = data.json()
+    for key in data:
+        name = key
+        url = data[key]
+        space = {}
+        space['name'] = name
+        space['url'] = url
+        spaces.append(space)
+
     for space in spaces:
         get_spaceapi_data(space)
     output(spaces)
+
 
 def output(spaces):
     out = {}
     for space in spaces:
         print(space)
         try:
-            out[space['name']] = { 'latitude': space['lat'], 'longitude': space['lon'] }
+            out[space['name']] = {'latitude': space['lat'],
+                                   'longitude': space['lon']}
         except:
+            print("no data for spacei %s" % space['name'])
+            print(space)
             pass
-    with open('markers.json', 'w') as markers:
-        markers.write(json.dumps(out))
 
+    with open('marker.json', 'w') as marker:
+        marker.write(json.dumps(out))
 
 
 def get_spaceapi_data(space):
+    requests.packages.urllib3.disable_warnings()
     data = requests.get(space['url'], verify=False)
     data = data.json()
-    print(space)
     try:
         space['lat'] = data['location']['lat']
         space['lon'] = data['location']['lon']
