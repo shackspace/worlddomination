0f62c078ad77fe44bc587647ebef3d06256b8b29
makefu <github@syntax-fehler.de>
2015-03-25 12:10:08 +0100

    update coordinates generator

--- coordinates.py
+++ coordinates.py
@@ -1,45 +1,39 @@
 #!/usr/bin/env python3
 
-import json
+import json,sys
 import requests
+directory_url = 'http://spaceapi.net/directory.json'
 
 def main():
-    spaces = []
-    with open("urls.txt", 'r') as file:
-        urls = file.readlines()
-        for line in urls:
-            url = line.strip()
-            url = url.split(':', 1)
-            space = {}
-            space['name'] = url[0]
-            space['url'] = url[1]
-            spaces.append(space)
-    for space in spaces:
-        get_spaceapi_data(space)
-    output(spaces)
-
-def output(spaces):
+    spaces = requests.get(directory_url,verify=False).json()
     out = {}
-    for space in spaces:
-        print(space)
+    for space,url in spaces.items():
+        sys.stdout.write(space)
         try:
-            out[space['name']] = { 'latitude': space['lat'], 'longitude': space['lon'] }
-        except:
-            pass
-    with open('markers.json', 'w') as markers:
-        markers.write(json.dumps(out))
+            ret = requests.get(url,verify=False).json()
+
+            try:
+                lat = ret['location']['lat'] 
+                lon = ret['location']['lon'] 
+            except:
+                lat = ret['lat']
+                lon = ret['lon']
+
+            try:
+                o = ret['open']
+            except:
+                o = False
+
+            out[space] = { 'latitude': lat, 
+                    'longitude': lon,
+                    'open': o }
+            print(" succeeded!")
+        except Exception as e:
+            print(" failed - url {} - {}".format(space,url,e))
 
 
-
-def get_spaceapi_data(space):
-    data = requests.get(space['url'], verify=False)
-    data = data.json()
-    print(space)
-    try:
-        space['lat'] = data['location']['lat']
-        space['lon'] = data['location']['lon']
-    except:
-        pass
+    with open('marker.json', 'w') as markers:
+        json.dump(out,marker)
 
 
 if __name__ == "__main__":
